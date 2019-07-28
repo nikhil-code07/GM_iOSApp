9b66201d7993668edf3bcef0bf4c99473ab14f42
Nikhil Murade <nsp.murade35@gmail.com>
2019-07-28 11:57:35 -0400

    Create UI tableView components in storyboard and associated UI classes
    Create tableView and tableView custom cell in storyboard and ViewController and TableViewCell classes to render it on UI

---
+++ GM_iOSApp/GM_iOSApp/UI Components/UserCommitsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCommitsTableViewCell.swift
+//  GM_iOSApp
+//
+//  Created by Nikhil Murade on 7/28/19.
+//  Copyright © 2019 Nikhil Murade. All rights reserved.
+//
+
+import UIKit
+
+class UserCommitsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
