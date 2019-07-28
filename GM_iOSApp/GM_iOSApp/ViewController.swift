f81ef9de15c87087c94bed6a8a4ad024f197289f
Nikhil Murade <nsp.murade35@gmail.com>
2019-07-27 23:25:24 -0400

    create skeleton project for GM iOS assignment

---
+++ GM_iOSApp/GM_iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GM_iOSApp
+//
+//  Created by Nikhil Murade on 7/27/19.
+//  Copyright © 2019 Nikhil Murade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GM_iOSApp/GM_iOSAppTests/GM_iOSAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  GM_iOSAppTests.swift
+//  GM_iOSAppTests
+//
+//  Created by Nikhil Murade on 7/27/19.
+//  Copyright © 2019 Nikhil Murade. All rights reserved.
+//
+
+import XCTest
+@testable import GM_iOSApp
+
+class GM_iOSAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
